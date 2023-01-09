f9eb35fa9befd906ee10646aeb81aa34b543ab56
Rutger Cappendijk <rutgercappendijk@gmail.com>
2023-01-09 10:20:24 +0100

    think mariadb works?

--- srcs/mariadb/create_users.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'rcappend'@'%' IDENTIFIED BY 'rcappend-password';
-GRANT ALL PRIVILEGES ON *.* TO 'rcappend'@'%';
-
-CREATE USER 'not_rcappend'@'%' IDENTIFIED BY 'not-rcappend-password';
--- GRANT ALL PRIVILEGES ON *.* TO 'user2'@'%';
