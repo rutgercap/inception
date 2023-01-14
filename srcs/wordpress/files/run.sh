979d485211ab27ee7f04241da4e1200ad0fe9e56
Rutger Cappendijk <rcappend@f1r2s20.codam.nl>
2023-01-14 15:15:47 +0100

    d

--- srcs/wordpress/files/run.sh
+++ srcs/wordpress/files/run.sh
@@ -16,7 +16,7 @@ echo "Wordpress: setting up..."
 cd /var/www/html;
 wp-cli core download --allow-root
 mv /var/www/wp-config.php /var/www/html/
-wp-cli core install --url="$DOMAIN_NAME/wordpress" --title="$WP_TITLE" --admin_user="$MYSQL_USER" --admin_password="$MYSQL_PASSWORD" --admin_email="$MYSQL_USER_EMAIL" --skip-email --allow-root
+wp-cli core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$MYSQL_USER" --admin_password="$MYSQL_PASSWORD" --admin_email="$MYSQL_USER_EMAIL" --skip-email --allow-root
 wp-cli user create $MYSQL_SECOND_USER $MYSQL_SECOND_USER_EMAIL --role=author --user_pass=$MYSQL_SECOND_PASSWORD --allow-root
 echo "Wordpress: set up!"
 
