c8dd0c948d91b93ecc193a2671229d6f40ce7ea1
Rutger Cappendijk <rcappend@f1r2s20.codam.nl>
2023-01-16 12:24:02 +0100

    changed ordeR

--- srcs/wordpress/files/run.sh
+++ srcs/wordpress/files/run.sh
@@ -15,9 +15,9 @@ touch /run/php/php7.3-fpm.pid;
 echo "Wordpress: setting up..."
 cd /var/www/html;
 wp-cli core download --allow-root
-wp-cli core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$MYSQL_USER" --admin_password="$MYSQL_PASSWORD" --admin_email="$MYSQL_USER_EMAIL" --skip-email --allow-root
 echo creating config
 wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+wp-cli core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$MYSQL_USER" --admin_password="$MYSQL_PASSWORD" --admin_email="$MYSQL_USER_EMAIL" --skip-email --allow-root
 wp-cli user create $MYSQL_SECOND_USER $MYSQL_SECOND_USER_EMAIL --role=author --user_pass=$MYSQL_SECOND_PASSWORD --allow-root
 echo "Wordpress: set up!"
 
